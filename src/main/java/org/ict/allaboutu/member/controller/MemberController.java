b6b7b26cfb2846930fcfc809db0d0f62e4053848
jeougun <skyjo95@gmail.com>
2023-11-30 17:22:47 +0900

    231130 jeongun

--- src/main/java/org/ict/allaboutu/member/controller/MemberController.java
+++ src/main/java/org/ict/allaboutu/member/controller/MemberController.java
@@ -2,14 +2,21 @@ package org.ict.allaboutu.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ict.allaboutu.member.service.MemberService;
+import org.ict.allaboutu.member.domain.Member;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("members")
+@RequestMapping
 public class MemberController {
-    
+
+    @PostMapping("/login")
+    public String loginMethod(@RequestBody Member member) {
+        log.info("/login : " + member.toString());
+        return null;
+    }
 }
