7ff6aad9ae3fb3ceb41d83a0866c67abe0f5e62c
sungyoung <choisy7120@naver.com>
2023-12-01 18:28:00 +0900

    sungyoung

--- src/main/java/org/ict/allaboutu/notice/service/NoticeService.java
+++ src/main/java/org/ict/allaboutu/notice/service/NoticeService.java
@@ -2,13 +2,19 @@ package org.ict.allaboutu.notice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ict.allaboutu.board.domain.Board;
 import org.ict.allaboutu.board.repository.BoardRepository;
 import org.ict.allaboutu.board.repository.CommentRepository;
+import org.ict.allaboutu.board.service.BoardDto;
 import org.ict.allaboutu.notice.domain.Notice;
 import org.ict.allaboutu.notice.repository.NoticeRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,4 +30,26 @@ public class NoticeService {
 
         return noticeList;
     }
+
+    public Notice getByNoticeId(Long noticeNum){
+        return noticeRepository.findById(noticeNum).get();
+    }
+
+    public Notice createNotice(Notice notice) {
+
+        Long noticeNum = noticeRepository.findMaxNoticeNumber() + 1;
+        notice.setNoticeNum(noticeNum);
+        notice.setWriteDate(Date.valueOf(LocalDate.now()));
+        notice.setReadCount(0L);
+
+
+        return  noticeRepository.save(notice);
+
+    }
+    public Notice noticeUp(@RequestBody Notice notice){
+        return noticeRepository.save(notice);
+    }
+
+
+
 }
