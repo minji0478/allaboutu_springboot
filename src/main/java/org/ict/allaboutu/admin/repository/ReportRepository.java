367cd60d21454acb21b05ec892a92f63136e46c1
minji <minjijang21@gmail.com>
2023-12-07 18:02:43 +0900

    minji push 20231207 springboot

--- src/main/java/org/ict/allaboutu/admin/repository/ReportRepository.java
+++ src/main/java/org/ict/allaboutu/admin/repository/ReportRepository.java
@@ -1,20 +1,22 @@
 package org.ict.allaboutu.admin.repository;
 
 import org.ict.allaboutu.admin.domain.Report;
+import org.ict.allaboutu.board.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
 //    @Query("SELECT r.reportCount FROM Report r INNER JOIN Member u ON r.userNum = u.userNum")
 //    List<Report> findAllByUserNum(@Param("userNum") Long userNum);
 
-    @Query("SELECT r FROM Report r WHERE r.boardNum = :boardNum")
-    Optional<Report> findByReportNum(@Param("boardNum") Long boardNum);
+//    @Query("SELECT r.boardNum FROM Report r WHERE r.reportNum = :reportNum")
+//    Report findByReportNum(@Param("reportNum") Long reportNum);
 
     @Query(value = "SELECT MAX(r.reportNum) FROM Report r")
     public Long findMaxReportNum();
+
+    @Query("SELECT r.boardNum FROM Report r WHERE r.reportNum = :reportNum")
+    Board findByReportNum(@Param("reportNum") Long reportNum);
 }
